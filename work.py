6c46f8641acb9dba035120883dea3fc727bc68dc
harutsaga <harutyunsaga@gmail.com>
2017-04-17 00:00:00 +0800

    Written 15 lines

--- work.py
+++ work.py
@@ -168,3 +168,18 @@ class webauto_base():
                         target.send_keys(Keys.CONTROL + "a")
                         target.send_keys(value)
                         break
+                    else:
+                        js = "arguments[0].value = '%s'" % (value)
+                        self.browser.execute_async_script(js, target)
+            return True
+        except Exception as e:
+            self.log_error(str(e))
+            return False
+
+    def wait_present(self, xpath, timeout = 2):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                try:
+                    target = self.browser.find_element_by_xpath(xpath)
