27ac3808ed2c96d34a028719302b7eee0ba4521f
harutsaga <harutyunsaga@gmail.com>
2017-08-20 00:00:00 +0800

    Written 25 lines

--- work.py
+++ work.py
@@ -783,3 +783,28 @@ class webauto_base():
     def expand_shadow_element(self, element):
         try:
             shadow_root = self.browser.execute_script('return arguments[0].shadowRoot', element)
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import *
+from selenium.common.exceptions import TimeoutException
+
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request as req
+
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+import api.states.python_anticaptcha as anticap
+ANTICAPTCHA_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+
