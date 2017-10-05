21160aad069c293da522f317a39ca34110a9a8a8
harutsaga <harutyunsaga@gmail.com>
2017-10-06 00:00:00 +0800

    Written 15 lines

--- work.py
+++ work.py
@@ -1028,3 +1028,18 @@ class webauto_base():
                         js = """
                             xpath = "%s";
                             y=document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                            y.click()
+                            """%(xpath)
+                        self.browser.execute_script(js)
+                    return True
+            return False
+        except Exception as e:
+            self.log_error(str(e))
+
+    def middle_click(self, xpath, timeout = 3):
+        js = """
+            xpath = "%s";
+            var mouseWheelClick = new MouseEvent('click', {'button': 1, 'which': 1 });
+            y=document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            y.dispatchEvent(mouseWheelClick)
+            """%(xpath)
