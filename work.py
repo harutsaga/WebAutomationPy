7f331cefdaac2600748a4c701213581301953bf9
harutsaga <harutyunsaga@gmail.com>
2018-05-14 00:00:00 +0800

    Written 20 lines

--- work.py
+++ work.py
@@ -2123,3 +2123,23 @@ class webauto_base():
 
     def middle_click(self, xpath, timeout = 3):
         js = """
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import *
+from selenium.common.exceptions import TimeoutException
+
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request as req
+
+import logging
