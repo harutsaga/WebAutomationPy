93de04c714e6e055169b898f1e862dd7e1065a93
harutsaga <harutyunsaga@gmail.com>
2018-12-12 00:00:00 +0800

    Written 5 lines

--- work.py
+++ work.py
@@ -3198,3 +3198,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import *
+from selenium.common.exceptions import TimeoutException
+
