ef9e6be77ee5fb73248b219d1723e47a91bd1953
harutsaga <harutyunsaga@gmail.com>
2019-01-02 00:00:00 +0800

    Written 30 lines

--- work.py
+++ work.py
@@ -3278,3 +3278,33 @@ class webauto_base():
         try:
             now = time.time()
             future = now + timeout
+            while time.time() < future:
+                pass
+            return True
+        except Exception as e:
+            return False
+
+    # let the browser to wait for <timeout> seconds
+    def delay(self, timeout = 3):
+        self.browser.implicitly_wait(timeout)
+        
+    # number of occurences for specified xpath
+    def occurence(self, xpath):
+        try:
+            elems = self.browser.find_elements_by_xpath(xpath)
+            return len(elems)
+        except:
+            return 0
+
+    # get base64 encoding of image from xpath
+    def get_base64_from_image(self, xpath_img):
+        try:
+            js = """
+                xpath="%s";
+                img=document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;             ;
+                var canvas = document.createElement('canvas');
+                canvas.width = img.width;
+                canvas.height = img.height;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0);
+                var dataURL = canvas.toDataURL('image/png');
