b7c5b8cde6d529336631c1720475c4466c39208c
harutsaga <harutyunsaga@gmail.com>
2019-03-07 00:00:00 +0800

    Written 20 lines

--- work.py
+++ work.py
@@ -3608,3 +3608,23 @@ class webauto_base():
             return False
 
     # check if there is an element in the specified xpath
+    def is_element_present(self, xpath):
+        try:
+            elem = self.browser.find_element_by_xpath(xpath)
+            if elem is None:
+                return False
+            return True
+        except:
+            return False
+
+    def enter_text(self, xpath, value, timeout = 3, manual = True):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                target = self.browser.find_element_by_xpath(xpath)
+                if target is not None:
+                    if manual:
+                        target.send_keys(Keys.CONTROL + "a")
+                        target.send_keys(value)
+                        break
