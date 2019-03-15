f3293af106478e8bb9fa73a44a06057a54d65266
harutsaga <harutyunsaga@gmail.com>
2019-03-16 00:00:00 +0800

    Written 30 lines

--- work.py
+++ work.py
@@ -3643,3 +3643,33 @@ class webauto_base():
             while time.time() < future:
                 try:
                     target = self.browser.find_element_by_xpath(xpath)
+                    if target is not None:
+                        return True
+                except:
+                    pass
+            return False
+        except Exception as e:
+            self.log_error(str(e))(str(e))
+            return False
+
+    def wait_unpresent(self, xpath, timeout = 3):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                try:
+                    target = self.browser.find_element_by_xpath(xpath)
+                    if target is None:
+                        return True
+                except:
+                    return True
+            return False
+        except Exception as e:
+            self.log_error(str(e))(str(e))
+            return False
+
+    def navigate(self, url):        
+        self.browser.get(url)
+
+    def get_attribute(self, xpath, attr = 'value'):
+        try:
