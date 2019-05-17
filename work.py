2685a1c2c77b30444854d2bc211998f5c2745e9a
harutsaga <harutyunsaga@gmail.com>
2019-05-18 00:00:00 +0800

    Written 20 lines

--- work.py
+++ work.py
@@ -3968,3 +3968,23 @@ class webauto_base():
                         {
                             node = document.evaluate("%s", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
                             if (node==null) 
+                                return '';
+                            node.%s='%s'; 
+                            return 'ok';
+                })()"""%(xpath,field,val)
+        # print(script)
+        self.browser.execute_script(script)
+
+    def click_element(self, xpath, timeout = 3, mode = 1):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                target = self.browser.find_element_by_xpath(xpath)
+                if target is not None:
+                    if mode == 0:
+                        target.click()
+                    elif mode == 1:
+                        js = """
+                            xpath = "%s";
+                            y=document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
