33b91bac2bfc08b0cebf8754992a8bee0168f9da
harutsaga <harutyunsaga@gmail.com>
2019-07-23 00:00:00 +0800

    Written 10 lines

--- work.py
+++ work.py
@@ -4308,3 +4308,13 @@ class webauto_base():
                 sr = self.expand_shadow_element(elem)
                 if sr is not None:
                     elem = sr.find_element_by_css_selector('settings-basic-page')
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
